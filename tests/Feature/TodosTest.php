6f86115d630e5bf04f7414cb92323c7c1f8bf280
abdelrahman-gad <abdelrahmangad95@gmail.com>
2021-03-11 20:54:56 +0200

    test alert connect to twitter

--- tests/Feature/TodosTest.php
+++ tests/Feature/TodosTest.php
@@ -15,6 +15,8 @@ class TodosTest extends TestCase
      * @return void
      */
     use RefreshDatabase;
+
+
     public function test_logged_in_user_can_see_todos_page()
     {
 
@@ -23,4 +25,31 @@ class TodosTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_logged_in_user_see_alert_to_connect_to_twitter()
+    {
+
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->get('/todo');
+
+        $response->assertSee('to publish your todos');
+    }
+
+    public function test_logged_in_user_can_add_todo()
+    {
+
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->post('/todo',[
+            'name'=>'simple todo'
+        ]);
+
+        // $response->assertStatus(302);
+
+
+        $this->assertDatabaseHas('todos',[
+            'user_id' => $user->id,
+            'name' => 'simple todo'
+        ]);
+    }
+
 }
