c135100d4fb62aea58c3bdcfe111fee55f27211b
abdelrahman-gad <abdelrahmangad95@gmail.com>
2021-03-12 17:42:42 +0200

    test_command_is_emailing_users_for_pending_todos

--- tests/Feature/BehindScheduleCommandTest.php
+++ tests/Feature/BehindScheduleCommandTest.php
@@ -7,8 +7,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Todo;
 use Illuminate\Support\Facades\Mail;
-
-
+use App\Mail\BehindScheduleTodosEmail;
 
 class BehindScheduleCommandTest extends TestCase
 {
@@ -17,10 +16,13 @@ class BehindScheduleCommandTest extends TestCase
     public function test_command_is_emailing_users_for_pending_todos()
     {
        Mail::fake();
-       factory(Todo::class)->create();
+       factory(Todo::class)->create([
+         'created_at' => new \DateTime('-1 day')
+       ]);
       
        $this->artisan('todos:behind-schedule');
       
-       Mail::assertNothingSent();
+     
+       Mail::assertSent(BehindScheduleTodosEmail::class);
     }
 }
